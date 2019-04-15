c6810f18c7eec5d51ad57deb4674f922518faec8
Jakub Knejzlik <jakub.knejzlik@gmail.com>
2019-04-15 11:05:04 +0200

    Add option to get token without verification

--- index.d.ts
+++ index.d.ts
@@ -2,6 +2,6 @@ import { NappJSService } from "nappjs";
 export default class NappJSJWT extends NappJSService {
     getToken(req: any): Promise<any>;
     checkJWTPermissions(req: any, resource: any): Promise<Boolean>;
-    private isEnabled();
-    private getConfigs();
+    private isEnabled;
+    private getConfigs;
 }
--- index.ts
+++ index.ts
@@ -1,9 +1,11 @@
-const createError = require("http-errors");
-import * as bluebird from "bluebird";
-import fetch from "node-fetch";
-const jwt = bluebird.promisifyAll(require("jsonwebtoken"));
+const createError = require('http-errors');
 
-import { NappJSService } from "nappjs";
+import * as bluebird from 'bluebird';
+
+import { NappJSService } from 'nappjs';
+import fetch from 'node-fetch';
+
+const jwt = bluebird.promisifyAll(require('jsonwebtoken'));
 
 interface JWTConfig {
   secret: string;
@@ -13,8 +15,8 @@ interface JWTConfig {
 let _configsCache: JWTConfig[] | null = null;
 
 export default class NappJSJWT extends NappJSService {
-  public async getToken(req) {
-    if (!await this.isEnabled()) {
+  public async getToken(req, verify = true) {
+    if (!(await this.isEnabled())) {
       return null;
     }
 
@@ -24,31 +26,40 @@ export default class NappJSJWT extends NappJSService {
     let token = req.query.access_token || req.headers.authorization;
 
     if (!token) {
-      throw createError(401, "access token missing");
+      throw createError(401, 'access token missing');
     }
 
-    token = token.replace("Bearer ", "");
-    try {
-      let configs = await this.getConfigs();
+    token = token.replace('Bearer ', '');
+    if (verify) {
+      try {
+        let configs = await this.getConfigs();
 
-      let latestError = null;
-      for (let config of configs) {
-        try {
-          let res = await jwt.verifyAsync(token, config.secret, config.options);
-          req.jwt_cache = res;
-          return res;
-        } catch (e) {
-          latestError = e;
+        let latestError = null;
+        for (let config of configs) {
+          try {
+            let res = await jwt.verifyAsync(
+              token,
+              config.secret,
+              config.options
+            );
+            req.jwt_cache = res;
+            return res;
+          } catch (e) {
+            latestError = e;
+          }
         }
+        throw latestError;
+      } catch (e) {
+        throw createError(401, e.message);
       }
-      throw latestError;
-    } catch (e) {
-      throw createError(401, e.message);
+    } else {
+      let res = await jwt.decode(token);
+      return res;
     }
   }
 
   public async checkJWTPermissions(req, resource): Promise<Boolean> {
-    if (!await this.isEnabled()) {
+    if (!(await this.isEnabled())) {
       return true;
     }
 
@@ -64,15 +75,15 @@ export default class NappJSJWT extends NappJSService {
       return false;
     }
 
-    permissions = permissions.split("\n");
+    permissions = permissions.split('\n');
 
     let valid = false;
     for (let permission of permissions) {
-      let [_rule, _resource] = permission.split("|");
+      let [_rule, _resource] = permission.split('|');
       if (!_rule || !_resource) continue;
-      let regepx = new RegExp("^" + _resource.replace(/\*/g, ".*") + "$");
+      let regepx = new RegExp('^' + _resource.replace(/\*/g, '.*') + '$');
       if (regepx.test(resource)) {
-        if (_rule == "deny") {
+        if (_rule == 'deny') {
           return false;
         } else {
           valid = true;
@@ -98,23 +109,23 @@ export default class NappJSJWT extends NappJSService {
     const JWT_CERTS_URL = process.env.JWT_CERTS_URL;
     const JWT_PUBLIC_CERT = process.env.JWT_PUBLIC_CERT;
 
-    if (typeof JWT_SECRET !== "undefined") {
-      configs.push({ secret: JWT_SECRET, options: { algorhitm: "HS256" } });
+    if (typeof JWT_SECRET !== 'undefined') {
+      configs.push({ secret: JWT_SECRET, options: { algorhitm: 'HS256' } });
     }
-    if (typeof JWT_PUBLIC_CERT !== "undefined") {
+    if (typeof JWT_PUBLIC_CERT !== 'undefined') {
       configs.push({
         secret: JWT_PUBLIC_CERT,
-        options: { algorhitm: "RS256" }
+        options: { algorhitm: 'RS256' }
       });
     }
-    if (typeof JWT_CERTS_URL !== "undefined") {
+    if (typeof JWT_CERTS_URL !== 'undefined') {
       let res = await fetch(JWT_CERTS_URL);
       let content = await res.json();
       configs = configs.concat(
         content.map(cert => {
           return {
-            secret: new Buffer(cert.key, "base64"),
-            options: { algorhitm: "RS256" }
+            secret: new Buffer(cert.key, 'base64'),
+            options: { algorhitm: 'RS256' }
           };
         })
       );
